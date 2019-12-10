20a327c435a33abaae15c413d2096397475a64e6
Hosni Ghribi <ghribi.hosni7@gmail.com>
2019-12-10 14:04:43 +0100

    Updates

---
+++ precache-manifest.59a60174b006a45124b94b117c2367ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "357cb591037c99d5ce84ce6364f1972a",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "6d52d4330d8eecfb1c97",
+    "url": "/react-movie-app/static/css/main.1ab83558.chunk.css"
+  },
+  {
+    "revision": "43596ef4779c872b2a22",
+    "url": "/react-movie-app/static/js/2.66e22e01.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-movie-app/static/js/2.66e22e01.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d52d4330d8eecfb1c97",
+    "url": "/react-movie-app/static/js/main.bfac4d43.chunk.js"
+  },
+  {
+    "revision": "065ede9c8a91280c1dc0",
+    "url": "/react-movie-app/static/js/runtime-main.43864add.js"
+  }
+]);
